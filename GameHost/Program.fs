bac8f902902b297c5116ea43ec346a8ddcba3b1e
Kurt Lippert <kurtlippert@gmail.com>
2025-11-01 23:25:20 -0400

    initial commit

---
+++ GameHost/Program.fs
@@ -0,0 +1,52 @@
+﻿open System
+open System.IO
+open System.Reflection
+open Raylib_cs
+open GameLogic
+open GameAbstractions
+
+let loadAssembly path =
+    let bytes = File.ReadAllBytes(path)
+    let asm = Assembly.Load(bytes)
+    let factoryType = asm.GetType("GameLogic.GameFactory")
+    let createMethod = factoryType.GetMethod("Create")
+    createMethod.Invoke(null, [||]) :?> IGame
+
+[<EntryPoint>]
+let main _ =
+    let logicDll =
+        Directory.GetFiles("GameLogic/bin/Debug", "GameLogic.dll", SearchOption.AllDirectories)
+        |> Array.head
+        |> Path.GetFullPath
+
+    let mutable game = loadAssembly logicDll
+    let mutable lastWrite = File.GetLastWriteTime(logicDll)
+
+    Raylib.InitWindow(800, 600, "F# Hot Reload")
+    Raylib.SetTargetFPS(60)
+
+    while not (Raylib.WindowShouldClose() |> CBool.op_Implicit) do
+        // Watch for rebuild
+        let currentWrite = File.GetLastWriteTime(logicDll)
+
+        if currentWrite > lastWrite then
+            lastWrite <- currentWrite
+
+            try
+                let state = game.SaveState()
+                let newGame = loadAssembly logicDll
+                newGame.LoadState(state)
+                game <- newGame
+            with ex ->
+                printfn "Reload failed: %A" ex
+
+        let dt = Raylib.GetFrameTime()
+        game.Update(dt)
+
+        Raylib.BeginDrawing()
+        Raylib.ClearBackground(Color.RayWhite)
+        game.Draw()
+        Raylib.EndDrawing()
+
+    Raylib.CloseWindow()
+    0
---
+++ GameLogic/Library.fs
@@ -0,0 +1,26 @@
+﻿namespace GameLogic
+
+open Raylib_cs
+open System.Numerics
+open GameAbstractions
+
+type SimpleGame() =
+    let mutable pos = Vector2(400.0f, 300.0f)
+    let speed = 100.0f
+
+    interface IGame with
+        member _.Update(dt) =
+            pos.X <- pos.X + speed * dt
+
+            if pos.X > (Raylib.GetScreenWidth() |> float32) then
+                pos.X <- 0f
+
+        member _.Draw() =
+            Raylib.DrawCircleV(pos, 30.0f, Color.Blue)
+
+        member _.SaveState() = box pos
+
+        member _.LoadState(state: obj) = pos <- unbox state
+
+module GameFactory =
+    let Create () : IGame = SimpleGame() :> IGame
